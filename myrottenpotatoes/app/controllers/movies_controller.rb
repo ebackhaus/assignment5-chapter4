f329e7495b7be63121722dce0b84077a8805e56d
Eric Backhaus <eric.backhaus@mnsu.edu>
2019-04-03 05:46:09 +0000

    Section 4.7 complete

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -14,4 +14,15 @@ class MoviesController < ApplicationController
     @movie = Movie.new
     # default: render 'new' template
   end
+  
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  
+  private
+  def movie_params
+    params.require(:movie).permit(:title,:rating,:release_date)
+  end
 end
